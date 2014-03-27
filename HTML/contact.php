bc40ccda7018da19edebea61d408adf3b73bf621
Lilliana Víquez Murillo <sittingducks@gmail.com>
2014-03-27 12:23:02 -0600

    adding styles and fonts

---
+++ HTML/contact.php
@@ -0,0 +1,15 @@
+<?php
+$to = "hello@twelveohsixcreative.com";
+$email = $_REQUEST['email'] ;
+$name = $_REQUEST['name'] ;
+$phone = $_REQUEST['phone'] ;
+$subject = "Message from: $name";
+$message = $_REQUEST['message'] ;
+$headers = "From: $email";
+$body = "From: $name \n\n Email: $email \n\n Phone number: $phone \n\n Message: $message";
+$sent = mail($to, $subject, $body, $headers) ;
+if($sent)
+{echo "Mail Sent";}
+else
+{echo "Error!";}
+?>
